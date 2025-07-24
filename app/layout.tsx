9236f3a8830de92665a86df763b5fc273abc3fc7
Le <lepha@amazon.com>
2025-07-24 17:47:28 +1000

    add login with google and facebook

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
           >
             <ConvexClientProvider>
               <ThemeModeToggle />
-              {children}
+                {children}
             </ConvexClientProvider>
           </ThemeProvider>
         </body>
--- convex/auth.ts
+++ convex/auth.ts
@@ -1,6 +1,26 @@
-import { Password } from "@convex-dev/auth/providers/Password";
-import { convexAuth } from "@convex-dev/auth/server";
+import Google from "@auth/core/providers/google";
+import Facebook from "@auth/core/providers/facebook";import { convexAuth } from "@convex-dev/auth/server";
+import { MutationCtx } from "./_generated/server";
+import { findUserByEmail } from "./users";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [Password],
+  providers: [Google, Facebook],
+  callbacks: {
+    // `args.type` is one of "oauth" | "email" | "phone" | "credentials" | "verification"
+    // `args.provider` is the currently used provider config
+    async createOrUpdateUser(ctx: MutationCtx, args) {
+      if (args.existingUserId) {
+        // Optionally merge updated fields into the existing user object here
+        return args.existingUserId;
+      }
+ 
+      // Implement your own account linking logic:
+      const existingUser = await findUserByEmail(ctx, args.profile.email);
+      if (existingUser) return existingUser._id;
+ 
+      return ctx.db.insert("users", {
+        email: args.profile.email,
+      });
+    },
+  },
 });
